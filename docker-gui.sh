e19718ccf1aea025356fe4f6e24cbff00af9081a
Tamal kanti mazumder <65080702+striketm98@users.noreply.github.com>
2022-07-02 01:04:33 +0530

    Create docker-gui.sh

---
+++ docker-gui.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo systemctl status docker
+sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
